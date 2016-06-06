f82196f4fc88cf59d12d035261d8b9d027ff0309
Matt Brown <matt@apii.io>
2016-06-06 18:17:17 +0100

    yaml config base added

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
 
+	"gopkg.in/yaml.v2"
+)
+
+// Config yaml parameters
+type Config struct {
+	Main struct {
+		APIPort string
+	}
+}
+
+func main() {
+	configFile, _ := filepath.Abs("./config.yml")
+	yamlFile, err := ioutil.ReadFile(configFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(yamlFile, &config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(config.Main.APIPort)
 }
